e4f194de42783bf32ae76835be0efcd996a1d9b6
Kai Hendry <hendry@iki.fi>
2023-12-19 17:14:50 +0000

    added quality :cry:

--- src/put-item.test.ts
+++ src/put-item.test.ts
@@ -1,42 +1,42 @@
-import type { APIGatewayProxyEventV2, Context } from 'aws-lambda'
+import type { APIGatewayProxyEventV2 } from 'aws-lambda'
 import { putItemHandler } from './put-item'
 import {
-  GetParameterCommand,
-  SSMClient
+    GetParameterCommand,
+    SSMClient
 } from '@aws-sdk/client-ssm'
 import { mockClient } from 'aws-sdk-client-mock'
 
 describe('Test putItemHandler', function () {
-  const ssmMock = mockClient(SSMClient)
+    const ssmMock = mockClient(SSMClient)
 
-  beforeEach(() => {
-    ssmMock.reset()
-  })
+    beforeEach(() => {
+        ssmMock.reset()
+    })
 
-  it('should handle PUT request', async () => {
-    const ssmResult = {
-      Parameter: {
-        Name: '/dev/message',
-        Value: 'hello from ssm!'
-      }
-    }
-    ssmMock.on(GetParameterCommand).resolves(ssmResult)
-
-    const result = await putItemHandler({
-      requestContext: {
-        http: {
-          method: 'POST',
-          path: '/api/unknown'
+    it('should handle PUT request', async () => {
+        const ssmResult = {
+            Parameter: {
+                Name: '/dev/message',
+                Value: 'hello from ssm!'
+            }
         }
-      }
-    } as APIGatewayProxyEventV2, {} as Context)
+        ssmMock.on(GetParameterCommand).resolves(ssmResult)
 
-    expect(result.statusCode).toBe(200)
+        const result = await putItemHandler({
+            requestContext: {
+                http: {
+                    method: 'POST',
+                    path: '/api/unknown'
+                }
+            }
+        } as APIGatewayProxyEventV2)
 
-    if (typeof result.body !== 'string') {
-      throw new Error('Result body is undefined or not a string')
-    }
-    const body = JSON.parse(result.body)
-    expect(body.message).toBe('hello from ssm!')
-  })
+        expect(result.statusCode).toBe(200)
+
+        if (typeof result.body !== 'string') {
+            throw new Error('Result body is undefined or not a string')
+        }
+        const body = JSON.parse(result.body)
+        expect(body.message).toBe('hello from ssm!')
+    })
 })
--- src/put-item.ts
+++ src/put-item.ts
@@ -1,7 +1,6 @@
 import {
-  type APIGatewayProxyEventV2,
-  type APIGatewayProxyStructuredResultV2,
-  type Context
+    type APIGatewayProxyEventV2,
+    type APIGatewayProxyStructuredResultV2,
 } from 'aws-lambda'
 
 import middy from '@middy/core'
@@ -14,67 +13,69 @@ import { getParameter } from '@aws-lambda-powertools/parameters/ssm'
 import { createError } from '@middy/util'
 
 export type SecretRetriever = (
-  environmentName: string,
-  parameterName: string
+    environmentName: string,
+    parameterName: string
 ) => Promise<string | null>
 
 export interface HandlerArgs {
-  environment?: string
-  secretRetriever?: SecretRetriever
+    environment?: string
+    secretRetriever?: SecretRetriever
 }
 
 const logger = new Logger({ serviceName: 'put-item' })
 
-async function logParameter (name: string): Promise<string> {
-  logger.debug('getting ssm parameter', { name })
-  return await (getParameter(name) as Promise<string>)
+async function logParameter(name: string): Promise<string> {
+    logger.debug('getting ssm parameter', { name })
+    return await (getParameter(name) as Promise<string>)
 }
 
-export async function putItemHandler (
-  event: APIGatewayProxyEventV2,
-  context: Context
+export async function putItemHandler(
+    event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyStructuredResultV2> {
-  logger.info('putItemHandler called')
+    logger.info('putItemHandler called')
 
-  if (event?.requestContext?.http?.method !== 'POST') {
-    logger.error(`LOG: postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`)
-    throw new Error(`postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`)
-  }
+    if (event?.requestContext?.http?.method !== 'POST') {
+        logger.error(`LOG: postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`)
+        throw new Error(`postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`)
+    }
 
-  if (event.queryStringParameters?.errorCode) {
-    logger.error(`LOG: errorCode: ${event.queryStringParameters?.errorCode}`)
-    const errorCode = parseInt(event.queryStringParameters?.errorCode)
-    throw createError(errorCode, 'random message')
-  }
+    const errorCodeStr = event.queryStringParameters?.errorCode ?? ''
+    if (errorCodeStr.trim()) {
+        logger.error(`LOG: errorCode: ${errorCodeStr}`)
+        const errorCode = parseInt(errorCodeStr)
+        throw createError(errorCode, 'random message')
+    }
 
-  if (event.queryStringParameters?.debugmsg) {
-    logger.debug(`LOG: debugmsg: ${event.queryStringParameters?.debugmsg}`)
-  }
+    const debugMsg = event.queryStringParameters?.debugmsg ?? ''
+    if (debugMsg.trim()) {
+        logger.debug(`LOG: debugmsg: ${debugMsg}`)
+    }
 
-  const parameter = await logParameter('/dev/message')
+    const parameter = await logParameter('/dev/message')
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      version: process.env.VERSION,
-      message: parameter
-    })
-  }
-};
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            version: process.env.VERSION,
+            message: parameter
+        })
+    }
+}
 
-export const newHandler = (args: HandlerArgs) => {
-  return middy<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2>()
-    .use(injectLambdaContext(logger))
-    .use(httpErrorHandler())
-    .handler(putItemHandler)
+export function newHandler(args: HandlerArgs) {
+    console.log('environment', args.environment)
+    return middy<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2>()
+        .use(injectLambdaContext(logger))
+        .use(httpErrorHandler())
+        .handler(putItemHandler)
 }
 
 export const handler = newHandler({
-  environment: process.env.ENV || ''
+    environment: process.env.ENV || ''
 })
 
-export async function ssmSecretRetriever (
-  parameter: string
+export async function ssmSecretRetriever(
+    parameter: string
 ): Promise<string | null> {
-  return (await getParameter(parameter, { decrypt: true })) ?? null
+    return (await getParameter(parameter, { decrypt: true })) ?? null
 }
