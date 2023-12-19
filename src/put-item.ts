9659089c527d812b9297c98ddbd7f0ff5ec61c4b
Kai Hendry <hendry@iki.fi>
2023-12-19 16:36:57 +0000

    pnpm create @eslint/config

--- src/put-item.ts
+++ src/put-item.ts
@@ -1,82 +1,80 @@
 import {
-    APIGatewayProxyEventV2,
-    APIGatewayProxyStructuredResultV2,
-    Context
-} from 'aws-lambda';
+  type APIGatewayProxyEventV2,
+  type APIGatewayProxyStructuredResultV2,
+  type Context
+} from 'aws-lambda'
 
-import middy from '@middy/core';
-import httpErrorHandler from '@middy/http-error-handler';
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
 
+import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
 
-import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger';
+import { getParameter } from '@aws-lambda-powertools/parameters/ssm'
 
-import { getParameter } from '@aws-lambda-powertools/parameters/ssm';
-
-import { createError } from '@middy/util';
+import { createError } from '@middy/util'
 
 export type SecretRetriever = (
-    environmentName: string,
-    parameterName: string
-) => Promise<string | null>;
+  environmentName: string,
+  parameterName: string
+) => Promise<string | null>
 
 export interface HandlerArgs {
-    environment?: string;
-    secretRetriever?: SecretRetriever;
+  environment?: string
+  secretRetriever?: SecretRetriever
 }
 
-const logger = new Logger({ serviceName: 'put-item' });
+const logger = new Logger({ serviceName: 'put-item' })
 
-function logParameter(name: string): Promise<string> {
-    logger.debug('getting ssm parameter', { name });
-    return getParameter(name) as Promise<string>;
+async function logParameter (name: string): Promise<string> {
+  logger.debug('getting ssm parameter', { name })
+  return await (getParameter(name) as Promise<string>)
 }
 
-export async function putItemHandler(
-    event: APIGatewayProxyEventV2,
-    context: Context
+export async function putItemHandler (
+  event: APIGatewayProxyEventV2,
+  context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> {
+  logger.info('putItemHandler called')
 
-    logger.info("putItemHandler called");
+  if (event?.requestContext?.http?.method !== 'POST') {
+    logger.error(`LOG: postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`)
+    throw new Error(`postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`)
+  }
 
-    if (event?.requestContext?.http?.method !== 'POST') {
-        logger.error(`LOG: postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`);
-        throw new Error(`postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`);
-    }
+  if (event.queryStringParameters?.errorCode) {
+    logger.error(`LOG: errorCode: ${event.queryStringParameters?.errorCode}`)
+    const errorCode = parseInt(event.queryStringParameters?.errorCode)
+    throw createError(errorCode, 'random message')
+  }
 
-    if (event.queryStringParameters?.['errorCode']) {
-        logger.error(`LOG: errorCode: ${event.queryStringParameters?.['errorCode']}`);
-        const errorCode = parseInt(event.queryStringParameters?.['errorCode']);
-        throw createError(errorCode, 'random message');
-    }
+  if (event.queryStringParameters?.debugmsg) {
+    logger.debug(`LOG: debugmsg: ${event.queryStringParameters?.debugmsg}`)
+  }
 
-    if (event.queryStringParameters?.['debugmsg']) {
-        logger.debug(`LOG: debugmsg: ${event.queryStringParameters?.['debugmsg']}`);
-    }
+  const parameter = await logParameter('/dev/message')
 
-    const parameter = await logParameter('/dev/message');
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            version: process.env['VERSION'],
-            message: parameter,
-        }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      version: process.env.VERSION,
+      message: parameter
+    })
+  }
 };
 
 export const newHandler = (args: HandlerArgs) => {
-    return middy<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2>()
-        .use(injectLambdaContext(logger))
-        .use(httpErrorHandler())
-        .handler(putItemHandler);
-};
+  return middy<APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2>()
+    .use(injectLambdaContext(logger))
+    .use(httpErrorHandler())
+    .handler(putItemHandler)
+}
 
 export const handler = newHandler({
-    environment: process.env['ENV'] || '',
-});
+  environment: process.env.ENV || ''
+})
 
-export async function ssmSecretRetriever(
-    parameter: string,
+export async function ssmSecretRetriever (
+  parameter: string
 ): Promise<string | null> {
-    return (await getParameter(parameter, { decrypt: true })) ?? null;
+  return (await getParameter(parameter, { decrypt: true })) ?? null
 }
