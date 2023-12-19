05363c8cfa4421d5cfb03fc1c0eb2c611e197410
Kai Hendry <hendry@iki.fi>
2023-12-19 17:41:46 +0000

    chore: pnpx  prettier . --write

--- src/put-item.test.ts
+++ src/put-item.test.ts
@@ -1,42 +1,39 @@
-import type { APIGatewayProxyEventV2 } from 'aws-lambda'
-import { putItemHandler } from './put-item'
-import {
-    GetParameterCommand,
-    SSMClient
-} from '@aws-sdk/client-ssm'
-import { mockClient } from 'aws-sdk-client-mock'
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+import { putItemHandler } from "./put-item";
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
+import { mockClient } from "aws-sdk-client-mock";
 
-describe('Test putItemHandler', function () {
-    const ssmMock = mockClient(SSMClient)
+describe("Test putItemHandler", function () {
+  const ssmMock = mockClient(SSMClient);
 
-    beforeEach(() => {
-        ssmMock.reset()
-    })
+  beforeEach(() => {
+    ssmMock.reset();
+  });
 
-    it('should handle PUT request', async () => {
-        const ssmResult = {
-            Parameter: {
-                Name: '/dev/message',
-                Value: 'hello from ssm!'
-            }
-        }
-        ssmMock.on(GetParameterCommand).resolves(ssmResult)
+  it("should handle PUT request", async () => {
+    const ssmResult = {
+      Parameter: {
+        Name: "/dev/message",
+        Value: "hello from ssm!",
+      },
+    };
+    ssmMock.on(GetParameterCommand).resolves(ssmResult);
 
-        const result = await putItemHandler({
-            requestContext: {
-                http: {
-                    method: 'POST',
-                    path: '/api/unknown'
-                }
-            }
-        } as APIGatewayProxyEventV2)
+    const result = await putItemHandler({
+      requestContext: {
+        http: {
+          method: "POST",
+          path: "/api/unknown",
+        },
+      },
+    } as APIGatewayProxyEventV2);
 
-        expect(result.statusCode).toBe(200)
+    expect(result.statusCode).toBe(200);
 
-        if (typeof result.body !== 'string') {
-            throw new Error('Result body is undefined or not a string')
-        }
-        const body = JSON.parse(result.body)
-        expect(body.message).toBe('hello from ssm!')
-    })
-})
+    if (typeof result.body !== "string") {
+      throw new Error("Result body is undefined or not a string");
+    }
+    const body = JSON.parse(result.body);
+    expect(body.message).toBe("hello from ssm!");
+  });
+});
