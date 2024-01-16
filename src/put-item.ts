cd515ebc3022cf26834bf2abc13865c282f973d9
Kai Hendry <hendry@iki.fi>
2024-01-16 12:45:39 +0000

    refactor into seperate file

--- src/put-item.ts
+++ src/put-item.ts
@@ -14,7 +14,7 @@ import { Tracer, captureLambdaHandler } from "@aws-lambda-powertools/tracer";
 
 import { SSMProvider } from "@aws-lambda-powertools/parameters/ssm";
 
-import { Segment, Subsegment, getSegment } from "aws-xray-sdk-core";
+import { captureNativeFetch } from "./captureNativeFetch";
 
 const tracer = new Tracer();
 captureNativeFetch();
@@ -58,7 +58,13 @@ export async function putItemHandler(
     logger.debug(`LOG: debugmsg: ${debugMsg}`);
   }
 
-  const response = await fetch("https://httpbin.org/delay/1");
+  const response = await fetch("https://httpbin.org/delay/3");
+  // add content-length to the trace segment
+  const contentLength = response.headers.get("content-length");
+  if (contentLength) {
+    logger.debug("content-length", { contentLength });
+    tracer.putMetadata("something", contentLength);
+  }
   const data = await response.json();
 
   tracer.putAnnotation("nonodefetch", true);
@@ -87,127 +93,3 @@ export function newHandler(args: HandlerArgs) {
 export const handler = newHandler({
   environment: process.env.ENV || "",
 });
-
-function captureNativeFetch(): void {
-  const fetch = globalThis.fetch;
-  globalThis.fetch = async function (resource, options) {
-    const headers = resolveHeaders(resource, options);
-    const traceHeader = headers?.get("X-Amzn-Trace-Id");
-
-    if (!traceHeader) {
-      const parent = getSegment();
-
-      if (parent) {
-        const url = resolveUrl(resource);
-        const method = resolveMethod(resource);
-        const { hostname } = new URL(url);
-        const subsegment = parent.notTraced
-          ? parent.addNewSubsegmentWithoutSampling(hostname)
-          : parent.addNewSubsegment(hostname);
-        const root = getRootSegment(parent);
-        subsegment.namespace = "remote";
-
-        if (!options) {
-          options = {};
-        }
-
-        if (!options.headers) {
-          options.headers = new Headers();
-        }
-
-        (options.headers as Headers).set(
-          "X-Amzn-Trace-Id",
-          `Root=${root.trace_id};Parent=${subsegment.id};Sampled=${
-            subsegment.notTraced ? 0 : 1
-          }`,
-        );
-
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore-next-line
-        subsegment.http = {
-          request: {
-            url,
-            method,
-          },
-        };
-
-        try {
-          const res = await fetch.call(globalThis, resource, options);
-          if (res.status === 429) {
-            subsegment.addThrottleFlag();
-          } else if (res.status >= 400 && res.status < 500) {
-            subsegment.addErrorFlag();
-          } else if (res.status >= 500) {
-            subsegment.addFaultFlag();
-          }
-
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore-next-line
-          subsegment.http = {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore-next-line
-            ...subsegment.http,
-            response: {
-              status: res.status,
-              ...(res.headers.has("content-length") && {
-                content_length: res.headers.get("content-length"),
-              }),
-            },
-          };
-          subsegment.close();
-
-          return res;
-        } catch (err) {
-          subsegment.close(err as Error | string);
-          throw err;
-        }
-      }
-    }
-
-    return await fetch.call(globalThis, resource, options);
-  };
-}
-
-const resolveHeaders = (
-  input: RequestInfo | URL,
-  options?: RequestInit,
-): Headers | undefined => {
-  if (input instanceof Request) {
-    return input.headers;
-  } else if (options?.headers) {
-    return new Headers(options.headers);
-  }
-
-  return undefined;
-};
-
-const resolveUrl = (input: RequestInfo | URL): string => {
-  if (input instanceof Request) {
-    return input.url;
-  } else if (input instanceof URL) {
-    return input.href;
-  }
-
-  return input;
-};
-
-const resolveMethod = (
-  input: RequestInfo | URL,
-  options?: RequestInit,
-): string => {
-  if (input instanceof Request) {
-    return input.method;
-  } else if (options?.method) {
-    return options.method;
-  }
-
-  return "GET";
-};
-
-const getRootSegment = (segment: Segment | Subsegment): Segment => {
-  if (segment instanceof Segment) {
-    return segment;
-  }
-
-  return segment.segment;
-};
