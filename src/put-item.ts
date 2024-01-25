29571a17d7ed08b20ebbe67b6353f24577285553
Kai Hendry <hendry@iki.fi>
2024-01-25 20:41:21 +0000

    simple configuration

--- src/put-item.ts
+++ src/put-item.ts
@@ -8,77 +8,50 @@ import httpErrorHandler from "@middy/http-error-handler";
 
 import { Logger, injectLambdaContext } from "@aws-lambda-powertools/logger";
 
-import { createError } from "@middy/util";
-
 import { Tracer, captureLambdaHandler } from "@aws-lambda-powertools/tracer";
 
 import { SSMProvider } from "@aws-lambda-powertools/parameters/ssm";
 
-import { captureNativeFetch } from "./captureNativeFetch";
-
 const tracer = new Tracer();
-captureNativeFetch();
 
 const ssmProvider = new SSMProvider();
 tracer.captureAWSv3Client(ssmProvider.client);
 
 const logger = new Logger();
 
-async function logParameter(name: string): Promise<string> {
-  const maxAge = 30;
-  logger.debug("getting ssm parameter", { name, maxAge });
+interface aadConfig {
+  thing1: string,
+  thing2: string,
+  thing3: string,
+  thing4: string,
+}
 
-  const result = (await ssmProvider.get(name, { maxAge })) ?? "";
-
-  // https://github.com/am29d/powertools-playground/blob/main/cdk-app/lib/app/tracer.ts#L23C4-L25C6
-  if (result) {
-    // this doesn't appear to set the annotation in the ssm operation :/
-    tracer.getSegment()?.addAnnotation('parameter', name);
-  }
-
-  return result;
+function getAadConfig(ssmValues: { [key: string]: string }): aadConfig {
+  return {
+    thing1: ssmValues["thing1"] || "default",
+    thing2: ssmValues["thing2"] || "default",
+    thing3: ssmValues["thing3"] || "default",
+    thing4: ssmValues["thing4"] || "default",
+  };
 }
 
 export async function putItemHandler(
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyStructuredResultV2> {
-  tracer.getSegment();
-  if (event?.requestContext?.http?.method !== "POST") {
-    logger.error("not a POST", { method: event?.requestContext?.http?.method });
-    throw new Error(
-      `postMethod only accepts POST method, you tried: ${event?.requestContext?.http?.method} method.`,
-    );
-  }
 
-  const errorCodeStr = event.queryStringParameters?.errorCode ?? "";
-  if (errorCodeStr.trim()) {
-    const errorCode = parseInt(errorCodeStr);
-    logger.error("triggering error", { errorCode });
-    throw createError(errorCode, `random message from ${process.env.VERSION}`);
-  }
-
-  const response = await fetch("https://httpbin.org/delay/1");
-  const contentLength = response.headers.get("content-length");
-  if (contentLength) {
-    logger.debug("content-length", { contentLength });
-    tracer.putMetadata("something", contentLength);
-  }
-  const data = await response.json();
-
-  tracer.putAnnotation("nonodefetch", true);
-
-  const msg1 = await logParameter("/dev/message");
-  const msg2 = await logParameter("/dev/message2");
+  const values = await ssmProvider.getMultiple("/product/test/bar");
+  logger.debug("got ssm parameters", { values });
+  const config = getAadConfig(values ?? {});
 
   return {
     statusCode: 200,
     headers: {
       "X-Amzn-Trace-Id": tracer.getRootXrayTraceId() ?? "",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       version: process.env.VERSION,
-      msg1, msg2,
-      fetch: data.url,
+      config
     }),
   };
 }
